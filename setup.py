120833ea0e3ea9cd5080c0a301ce8f0d13f2c94e
zaxcie <forest.kevin@gmail.com>
2021-07-04 00:27:41 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Kaggle competititon Optiver',
+    author='Kevin Forest',
+    license='',
+)
